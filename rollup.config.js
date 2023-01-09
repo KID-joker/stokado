e372411838931496a650cd221dd0327191978e12
KID-joker <kid_joker@126.com>
2023-01-09 23:29:01 +0800

    chore: create playground html

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import typescript2 from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 import dts from "rollup-plugin-dts";
+import html from '@rollup/plugin-html';
 import path from 'path';
 
 export default [{
-  input: 'src/index.ts',
+  input: path.resolve(__dirname, 'src/index.ts'),
   output: {
     file: 'dist/proxy-web-storage.js',
     format: 'umd',
@@ -12,7 +13,11 @@ export default [{
   },
   plugins: [
     typescript2(),
-    terser()
+    terser(),
+    html({
+      fileName: 'playground/index.html',
+      publicPath: '../'
+    })
   ]
 }, {
   input: path.resolve(__dirname, 'src/index.ts'),
